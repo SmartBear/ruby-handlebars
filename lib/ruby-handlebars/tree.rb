36f087366a9b611072944baaeb63e183494fa73f
d316 <56690148+d316@users.noreply.github.com>
2019-10-21 01:29:57 -0700

    Ensure partial arguments are an array

--- lib/ruby-handlebars/tree.rb
+++ lib/ruby-handlebars/tree.rb
@@ -80,7 +80,7 @@ module Handlebars
 
     class PartialWithArgs < TreeItem.new(:partial_name, :arguments)
       def _eval(context)
-        hash = Hash[arguments.map(&:values).map do |vals| 
+        hash = Hash[[arguments].flatten.map(&:values).map do |vals| 
           [vals.first.to_s, vals.last._eval(context)]
         end]
         context.get_partial(partial_name.to_s).call hash
